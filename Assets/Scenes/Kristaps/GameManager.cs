cdc3d87ad2fd25196d1b3e2003d5ede6c589e5f3
krispchip <kristaps.zellitis@gmail.com>
2019-02-23 19:58:27 +0100

    Kriss commit

---
+++ Assets/Scenes/Kristaps/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    
+
+    void Awake()
+    {
+        Object.DontDestroyOnLoad(transform.gameObject);
+        
+    }
+
+
+    //SINGLETON
+    private static GameManager instance = null;
+
+
+    public static GameManager GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new GameManager();
+        }
+
+        return instance;
+    }
+
+}
---
+++ Assets/Scenes/Kristaps/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+
+
+    public static int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !MenuManager.gameIsOver) {
+
+            print("clicked");
+
+            score++;
+        }
+    }
+}
